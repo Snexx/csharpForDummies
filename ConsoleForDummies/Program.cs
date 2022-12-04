35035ff95d7d79d9ef3ba4d87eb123061c94cc6e
Patricio Veliz <pveliz@xms.cl>
2022-12-04 19:31:59 -0300

    Ejerciciosenclase

--- ConsoleForDummies/Program.cs
+++ ConsoleForDummies/Program.cs
@@ -8,9 +8,59 @@ namespace ConsoleForDummies
 {
     class Program
     {
+        //Consola principal
         static void Main(string[] args)
         {
-            //ExampleCommit
+
+            int intValue1 = 2;
+            int intValue2 = 3;
+            string stringValue1 = "2";
+            string stringValue2 = "3";
+            bool boolValue1 = false;
+            bool boolValue2 = true;
+            double doubleValue1 = 3.5;
+            double doubleValue2 = 4.5;
+            char charValue1 = 'k';
+            char charValue2 = 'j';
+
+            //Como escribir en una consola
+            //Console.WriteLine("hola1");
+
+            //Como leer en una consola 
+            //Console.ReadLine();
+
+            //Escribir en una consola la suma de los valores int (crear una variable previa)
+            //int suma1 = intValue1+ intValue2;
+            //Console.WriteLine(suma1);
+            //Console.ReadKey();
+
+            //Crear un valor de texto, ingresar un numero y crear un campo tipo numero con el
+            //valor ingresado en el texto
+            //string resta1 = "4";
+            //int ej4 = Convert.ToInt32(resta1);
+            //Console.WriteLine(ej4);
+            //Console.ReadKey();
+
+            //Crear un valor de texto, ingresar un valor Verdadero o falso, y crear un campo tipo bool con
+            //el valor ingresado en el texto
+            //string ej5 = "true";
+            //bool transformacion = Convert.ToBoolean(ej5);
+            //Console.WriteLine(transformacion);
+
+
+            //Pedir por consola que ingrese un animal, posteriormente un numero y para finalizar un bool, guardar las 3 variables e
+            //imprimir un texto con los 3 datos registrados por el usuario.
+
+            Console.WriteLine("ingrese un animal");
+            var animalConsola =  Console.ReadLine();
+            Console.WriteLine("ingrese un numero");
+            var numeroConsola = Console.ReadLine();
+            Console.WriteLine("ingrese un bool");
+            var boolConsola = Console.ReadLine();
+
+            Console.WriteLine(animalConsola+" "+numeroConsola+" "+boolConsola);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            //1.- Crear variables para asignar nombres y apellidos
+            Console.WriteLine("A continuación favor ingresar sus nombres y apellido separados");
+            //Solicitar nombres y apellidos y guardarlos en los valores previamente creados.
+
+            //Imprimir resultado (Apellidos + nombre)
+            //Console.WriteLine("Su nombre ordenado por apellidos es: ");
+
+
+            Console.ReadKey();
+
+
+            //2
+
+
+            //Crear una consola que solicite el valor faltante y que imprima el resultado
+            const int firstInt = 15;
+            //Console.WriteLine("Que numero sumado a " + firstInt + " da 20");
+            //CODIGO AQUI
+
+            //Imprimir respuesta final
+            //Console.WriteLine("El numero es: ");
+
+            //Console.ReadKey();
+
+
+
+            //Ejercicio 3
+
+            //1.- Crear una consola que tenga un texto inicial de bienvenida
+
+            //2.- Crear un valor fijo del 1 al 9
+
+            //3.- escribir en consola el siguiente texto "Su numero de la suerte es" y adicionar el numero creado
+
+            //4.- Crear una variable  con el valor faltanta para llegar a 7777 (Ej. valor inicial = 0 + 7777, valor faltante 7777)
+            //Este valor no puede ser ingresado manualmente (ej. (int number2 = 7777))
+
+            //5.- Imprimir un texto que indique el numer faltante
+
+            //Console.ReadKey();
+
+
+
+
+
+            //Ejerciio 4
+
+            //1.- Crear una consola que tenga un texto inicial de bienvenida, que solicite 2 numeros (el 2 numero debe solictar
+            //que sea mayor que el primero) y guardar ambas respuestas en variables de tipo numero, posteriormente debe indicar
+            //por cuanto es mayor el numero 2 en referencia al numero 1 e imprimirlo en pantalla.
+
+            Console.ReadKey();
+        }
+    }
+}
