5bd0fd5bea0bce7c65ca2d6cb5a758f807676d3e
Patricio Veliz <pveliz@xms.cl>
2022-12-09 09:35:49 -0300

    test

--- ConsoleForDummies/Program.cs
+++ ConsoleForDummies/Program.cs
@@ -11,56 +11,159 @@ namespace ConsoleForDummies
         //Consola principal
         static void Main(string[] args)
         {
+            #region Primer Clase
 
-            int intValue1 = 2;
-            int intValue2 = 3;
-            string stringValue1 = "2";
-            string stringValue2 = "3";
-            bool boolValue1 = false;
-            bool boolValue2 = true;
-            double doubleValue1 = 3.5;
-            double doubleValue2 = 4.5;
-            char charValue1 = 'k';
-            char charValue2 = 'j';
+            //int intValue1 = 2;
+            //int intValue2 = 3;
+            //string stringValue1 = "2";
+            //string stringValue2 = "3";
+            //bool boolValue1 = false;
+            //bool boolValue2 = true;
+            //double doubleValue1 = 3.5;
+            //double doubleValue2 = 4.5;
+            //char charValue1 = 'k';
+            //char charValue2 = 'j';
 
-            //Como escribir en una consola
-            //Console.WriteLine("hola1");
+            ////Como escribir en una consola
+            ////Console.WriteLine("hola1");
 
-            //Como leer en una consola 
-            //Console.ReadLine();
+            ////Como leer en una consola 
+            ////Console.ReadLine();
 
-            //Escribir en una consola la suma de los valores int (crear una variable previa)
-            //int suma1 = intValue1+ intValue2;
-            //Console.WriteLine(suma1);
+            ////Escribir en una consola la suma de los valores int (crear una variable previa)
+            ////int suma1 = intValue1+ intValue2;
+            ////Console.WriteLine(suma1);
+            ////Console.ReadKey();
+
+            ////Crear un valor de texto, ingresar un numero y crear un campo tipo numero con el
+            ////valor ingresado en el texto
+            ////string resta1 = "4";
+            ////int ej4 = Convert.ToInt32(resta1);
+            ////Console.WriteLine(ej4);
+            ////Console.ReadKey();
+
+            ////Crear un valor de texto, ingresar un valor Verdadero o falso, y crear un campo tipo bool con
+            ////el valor ingresado en el texto
+            ////string ej5 = "true";
+            ////bool transformacion = Convert.ToBoolean(ej5);
+            ////Console.WriteLine(transformacion);
+
+
+            ////Pedir por consola que ingrese un animal, posteriormente un numero y para finalizar un bool, guardar las 3 variables e
+            ////imprimir un texto con los 3 datos registrados por el usuario.
+
+            //Console.WriteLine("ingrese un animal");
+            //string animalConsola =  Console.ReadLine();
+            //Console.WriteLine("ingrese un numero");
+            //string numeroConsola = Console.ReadLine();
+            //Console.WriteLine("ingrese un bool");
+            //string boolConsola = Console.ReadLine();
+
+            //Console.WriteLine(animalConsola+" "+numeroConsola+" "+boolConsola);
+
+            ////Console.ReadKey();
+            #endregion
+
+            ////Arreglo con 4 valores 
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Peras","asffas","asfasf","asfasf"}; //0-1242
+
+            ////Obtener cantidad de valores en numero
+            //int valores = cars.Length; //1243
+
+            ////Pedir que ingrese un valor de auto
+            //Console.WriteLine("Ingrese un valor de auto");
+
+            ////Valores permitidos
+            //Console.WriteLine("Valores permitidos: 1 - " + valores); // ingresal del 1 al 1243
+            //int valorR = Convert.ToInt32(Console.ReadLine()); //1243
+            //int valorLista = valorR - 1; //1242
+
+            //if (valorR > valores || valorR <= 0) //(1243 > 1243 || 1243 <= 0 ) NO
+            //{
+            //    Console.WriteLine("Valor NO permitido");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(cars[valorLista]); //POSICION DEL ARRAY 1242
+            //}
+
+            ////0-1-2-3-4-5
+            ////1-2-3-4-5-6
+
             //Console.ReadKey();
 
-            //Crear un valor de texto, ingresar un numero y crear un campo tipo numero con el
-            //valor ingresado en el texto
-            //string resta1 = "4";
-            //int ej4 = Convert.ToInt32(resta1);
-            //Console.WriteLine(ej4);
+            //string[] numerosEJ1 = new string[4];  //Inicializar sin valores
+            //string[] numerosEJ2 = { "", "", "", ""}; //Inicializar sin valores
+            //string[] numerosEJ3 = new string[] { "", "", "", "" }; //Inicializar sin valores
+
+
+            //FOR
+
+            //for (int i = 1; i <= 9; ++i)
+            //{
+            //    Console.WriteLine("Alumno N°: " + i);
+            //    Console.WriteLine("Nombre: ");
+            //    Console.WriteLine("Apellido: ");
+            //    Console.WriteLine("Edad: ");// Executes 2 times
+
+            //    // Inner loop
+            //    for (int j = 1; j <= 4; j++)
+            //    {
+            //        Console.WriteLine(" Cartola semastral Semestre: " + j);  // Executes 6 times (2 * 3)
+            //        for (int n = 1; n <= 10; n++)
+            //        {
+            //            Console.WriteLine("  Nota n°: " + j);  // Executes 6 times (2 * 3)
+            //        }
+            //    }
+            //    Console.WriteLine(" ");
+            //    Console.WriteLine(" ");
+            //    Console.WriteLine(" ");
+            //}
+
+            string[] autos = { "Juan", "Pedro", "Rodrigo", "" };
+            Array.Sort(autos);
+            foreach (string auto in autos)
+            {
+                Console.WriteLine(auto);
+            }
+
+            #region Continue
+            //Console.WriteLine("Ingrese un numero");
+            //int numerConsola = Convert.ToInt32(Console.ReadLine());
+
+
+            //int[] ListaNumeros2 = { 5000, 6000, 9000,12000,7000,15000 };
+            //foreach (int numero in ListaNumeros2)
+            //{
+
+            //    if (numero <= numerConsola)
+            //    {
+            //        continue;
+            //    }
+            //    Console.Write(" " + numero + " ");
+            //}
+            //Console.Write("For terminado");
             //Console.ReadKey();
+            #endregion
 
-            //Crear un valor de texto, ingresar un valor Verdadero o falso, y crear un campo tipo bool con
-            //el valor ingresado en el texto
-            //string ej5 = "true";
-            //bool transformacion = Convert.ToBoolean(ej5);
-            //Console.WriteLine(transformacion);
+            #region Break
+            //int[] ListaNumeros = { 10000, 11000, 9000, 15000 };
+            //foreach (int numero in ListaNumeros)
+            //{
+            //    if (numero < 10000)
+            //    {
+            //        break;
+            //    }
+            //    Console.Write(" " + numero + " ");
+            //}
+            //Console.Write("For terminado");
+            #endregion
 
-
-            //Pedir por consola que ingrese un animal, posteriormente un numero y para finalizar un bool, guardar las 3 variables e
-            //imprimir un texto con los 3 datos registrados por el usuario.
-
-            Console.WriteLine("ingrese un animal");
-            string animalConsola =  Console.ReadLine();
-            Console.WriteLine("ingrese un numero");
-            string numeroConsola = Console.ReadLine();
-            Console.WriteLine("ingrese un bool");
-            string boolConsola = Console.ReadLine();
-
-            Console.WriteLine(animalConsola+" "+numeroConsola+" "+boolConsola);
-
-            //Console.ReadKey();
+            #region Sort
+            //string[] autosSort = { "Juan", "Pedro", "Rodrigo", "" };
+            //Array.Sort(autosSort);
+            #endregion
+            Console.ReadKey();
         }
     }
 }
--- MyFirstConsole/Program.cs
+++ MyFirstConsole/Program.cs
@@ -10,130 +10,14 @@ namespace MyFirstConsole
     {
         static void Main(string[] args)
         {
-            //1
-
-            //1.- Crear variables para asignar nombres y apellidos
-             string nombre1 = "";
-             string apellido1 = "";
-
-
-            //Solicitar nombres y apellidos y guardarlos en los valores previamente creados.
-            //Console.WriteLine("ingrese nombre");
-            //nombre1 = Console.ReadLine();
-            //Console.WriteLine("ingrese apellido");
-            //apellido1 = Console.ReadLine();
-
-
-
-
-
-            //Imprimir resultado (Apellidos + nombre)
-            //Console.WriteLine("Su nombre ordenado por apellidos es: ");
-
-            //Console.WriteLine(apellido1 + " " + nombre1);
-
-
-            //Console.ReadKey();
-
-
-            //2
-
-
-            //Crear una consola que solicite el valor faltante y que imprima el resultado
-            //const int firstInt = 15;
-            //Console.WriteLine("Que numero sumado a " + firstInt + " da 20");
-            //CODIGO AQUI 
-            //Console.WriteLine("ingrese numero");
-            //string numero1 = Console.ReadLine();
-            //int numero2 = Convert.ToInt32(numero1);
-            //Console.WriteLine(numero2 + firstInt);
-
-
-
-
-
-            //Imprimir respuesta final
-            //Console.WriteLine("El numero es: ");
-
-            //Console.ReadKey();
-
-
-
-            //Ejercicio 3
-
-            //1.- Crear una consola que tenga un texto inicial de bienvenida
-
-            //2.- Crear un valor fijo del 1 al 9
-
-            //3.- escribir en consola el siguiente texto "Su numero de la suerte es" y adicionar el numero creado
-
-            //4.- Crear una variable  con el valor faltanta para llegar a 7777 (Ej. valor inicial = 0 + 7777, valor faltante 7777)
-            //Este valor no puede ser ingresado manualmente (ej. (int number2 = 7777))
-
-            //5.- Imprimir un texto que indique el numer faltante
-            //string saludo= "bienvenido";
-            //Console.WriteLine(saludo);
-            //int ej4 = 9;
-            // string suerte = "su numero de la suerte es";
-            // Console.WriteLine(suerte + " " + ej4);
-            // int resta1 = 7777-ej4;
-            // Console.WriteLine(resta1);
-            // Console.WriteLine(ej4 + resta1);
-
-
-            // Console.ReadKey();
-
-
-
-
-
-            //Ejercio 4
-
-            //1.- Crear una consola que tenga un texto inicial de bienvenida, que solicite 2 numeros (el 2 numero debe solictar
-            //que sea mayor que el primero) y guardar ambas respuestas en variables de tipo numero, posteriormente debe indicar
-            //por cuanto es mayor el numero 2 en referencia al numero 1 e imprimirlo en pantalla.
-
-            // 
-            // saludar
-            // solicitar 2 numeros, el segundo mayor
-            //guardar variables de int
-            //restar el 1 y 2 y ejecutar
-
-            Console.WriteLine("bienvenido");
-            string texto0 = "ingrese un numero";
-            Console.WriteLine(texto0);
-            string texto1 = Console.ReadLine();
-            int numero1 = Convert.ToInt32(texto1);
-            Console.WriteLine(numero1);
-            string valor = "ingrese un valor superior al primero";
-            Console.WriteLine(valor);
-            string texto2 = Console.ReadLine();
-            int numero2 = Convert.ToInt32(texto2);
-            int numer3 = numero2 - numero1;
-            Console.WriteLine("el numero: "+numero2+" es mayor por "+numer3);
-
-
-
-
-
-           Console.ReadKey();
+            //MyMethod();
+            Console.WriteLine("I just got executed!");
+            Console.ReadKey();
         }
-    }       #region Tarea 5
-            // Se necesita que el usuario ingrese su edad y se valide si es mayor de edad
-            #endregion
 
-            #region Tarea 6
-            // Se necesita que el usuario ingrese la cantidad de personas que componen
-            // su familia para validar si cumple con los requisitos para el bono, si son mas de 4 integrantes recibe $250000,
-            //si son mas de 6 recibe $500000, si no cumple con ninguna de las condiciones no recibe bono
-            #endregion
-
-            #region Tarea 7
-            // se necesita que el usuario ingrese la cantidad de kilos de la maleta, si pesa mas de 21
-            // requerira pagar un extra por la maleta, si no puede pasar sin recargo
-            #endregion
-
-            #region Tarea 8
-            //solicitar que el usuario ingrese 4 numeros, al finalizar indicar cual de los 4 numeros es el mayor.
-            #endregion
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+    }      
 }
